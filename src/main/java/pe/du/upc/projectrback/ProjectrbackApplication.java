ffc3f235d0df5218eec766029b80181c2568b1f0
julixnss <u20221f958@upc.edu.pe>
2025-08-08 10:24:52 -0500

    first commit

---
+++ src/main/java/pe/du/upc/projectrback/ProjectrbackApplication.java
@@ -0,0 +1,13 @@
+package pe.du.upc.projectrback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectrbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectrbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/du/upc/projectrback/ProjectrbackApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.du.upc.projectrback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectrbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
